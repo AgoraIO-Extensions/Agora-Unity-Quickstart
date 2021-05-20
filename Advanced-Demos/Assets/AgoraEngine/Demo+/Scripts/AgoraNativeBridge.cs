d67790a3d4d5919435da3c36c0024a441068efef
Rick Cheng <rick@agora.io>
2021-05-19 17:34:51 -0700

    Updated code for Windows ScreenSharing supporting Windows IDs

--- Advanced-Demos/Assets/AgoraEngine/Demo+/Scripts/AgoraNativeBridge.cs
+++ Advanced-Demos/Assets/AgoraEngine/Demo+/Scripts/AgoraNativeBridge.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using UnityEngine;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -14,6 +15,134 @@ public static class AgoraNativeBridge
     // the executable, so we have to use __Internal as the
     // library name.
     //     [DllImport ("__Internal")]
+
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+    // Save window titles and handles in these lists.
+    private static Dictionary<string, IntPtr> windowInfo;
+    private static List<MonitorInfoWithHandle> displayInfo;
+
+    public static MonitorInfoWithHandle[] GetWinDisplayInfo()
+    {
+        displayInfo = new List<MonitorInfoWithHandle>();
+        EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, FilterDisplayCallback, IntPtr.Zero);
+        return displayInfo.ToArray();
+    }
+
+    // Return a list of the desktop windows' handles and titles.
+    public static void GetDesktopWindowHandlesAndTitles(
+        out Dictionary<string, IntPtr> info)
+    {
+        windowInfo = new Dictionary<string, IntPtr>();
+
+        if (!EnumDesktopWindows(IntPtr.Zero, FilterCallback,
+            IntPtr.Zero))
+        {
+            info = null;
+        }
+        else
+        {
+            info = windowInfo;
+        }
+    }
+
+    // We use this function to filter windows.
+    // This version selects visible windows that have titles.
+    private static bool FilterCallback(IntPtr hWnd, int lParam)
+    {
+        // Get the window's title.
+        StringBuilder sb_title = new StringBuilder(1024);
+        GetWindowText(hWnd, sb_title, sb_title.Capacity);
+        string title = sb_title.ToString();
+
+        // If the window is visible and has a title, save it.
+        if (IsWindowVisible(hWnd) &&
+            string.IsNullOrEmpty(title) == false)
+        {
+            if (windowInfo.ContainsKey(title)) title = string.Format("{0}{1}", title, hWnd);
+            windowInfo.Add(title, hWnd);
+        }
+
+        // Return true to indicate that we
+        // should continue enumerating windows.
+        return true;
+    }
+
+    private static bool FilterDisplayCallback(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData)
+    {
+        var mi = new MONITORINFO();
+        mi.size = (uint) Marshal.SizeOf(mi);
+        GetMonitorInfo(hMonitor, ref mi);
+
+        // Add to monitor info
+        displayInfo.Add(new MonitorInfoWithHandle(hMonitor, mi));
+        return true;
+    }
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool IsWindowVisible(IntPtr hWnd);
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowText",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern int GetWindowText(IntPtr hWnd,
+        StringBuilder lpWindowText, int nMaxCount);
+
+    [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern bool EnumDesktopWindows(IntPtr hDesktop,
+        EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+
+    [DllImport("user32.dll", EntryPoint = "EnumDisplayMonitors",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip,
+        EnumMonitorsDelegate lpEnumCallbackFunction, IntPtr dwData);
+
+    [DllImport("user32.dll", EntryPoint = "GetMonitorInfo",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+    private static extern bool GetMonitorInfo(IntPtr hmon, ref MONITORINFO mi);
+
+    // Define the callback delegate's type.
+    private delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+    private delegate bool EnumMonitorsDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
+
+    // Monitor information with handle interface.
+    public interface IMonitorInfoWithHandle
+    {
+        IntPtr MonitorHandle { get; }
+        MONITORINFO MonitorInfo { get; }
+    }
+
+    public class MonitorInfoWithHandle : IMonitorInfoWithHandle
+    {
+        public IntPtr MonitorHandle { get; private set; }
+        public MONITORINFO MonitorInfo { get; private set; }
+
+        public MonitorInfoWithHandle(IntPtr monitorHandle, MONITORINFO monitorInfo)
+        {
+            MonitorHandle = monitorHandle;
+            MonitorInfo = monitorInfo;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MONITORINFO
+    {
+        public uint size;
+        public RECT monitor;
+        public RECT work;
+        public uint flags;
+    }
+#endif
 
 #if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
     // Other platforms load plugins dynamically, so pass the name
@@ -67,11 +196,10 @@ public static class AgoraNativeBridge
             Debug.LogWarning("JSON list count = " + macWindows.windows.Count);
             foreach (MacWindowModel win in macWindows.windows)
             {
-//                Debug.LogWarning(win.kCGWindowOwnerName + " => " + win.kCGWindowNumber);
+                Debug.LogWarning(win.kCGWindowOwnerName + " => " + win.kCGWindowNumber);
             }
         }
         return macWindows;
     }
 #endif
-
 }
--- Advanced-Demos/Assets/AgoraEngine/Demo+/Scripts/DesktopScreenShare.cs
+++ Advanced-Demos/Assets/AgoraEngine/Demo+/Scripts/DesktopScreenShare.cs
@@ -6,15 +6,20 @@ using System.Linq;
 using agora_gaming_rtc;
 using AgoraNative;
 
-// this is an example of using Agora Unity SDK
-// It demonstrates:
-// How to enable video
-// How to join/leave channel
-// 
+/// <summary>
+/// this is an example of using ScreenSharing APIs for Desktops
+/// </summary>
 public class DesktopScreenShare : PlayerViewControllerBase
 {
 
     Dropdown WindowOptionDropdown;
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+    readonly List<AgoraNativeBridge.RECT> WinDisplays = new List<AgoraNativeBridge.RECT>();
+#else
+    List<uint> MacDisplays;
+#endif
+    int CurrentDisplay = 0;
 
     protected override void SetupUI()
     {
@@ -24,17 +29,33 @@ public class DesktopScreenShare : PlayerViewControllerBase
         if (dropdown != null)
         {
 #if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
+            MacDisplays = AgoraNativeBridge.GetMacDisplayIds();
             WindowList list = AgoraNativeBridge.GetMacWindowList();
             if (list != null)
             {
                 dropdown.options = list.windows.Select(w =>
-                    new Dropdown.OptionData(w.kCGWindowOwnerName + "|" + w.kCGWindowNumber)).ToList();
+                    new Dropdown.OptionData(w.kCGWindowOwnerName + " | " + w.kCGWindowNumber)).ToList();
             }
-            GameObject.Find("InputField").SetActive(false);
-            GameObject.Find("WinHelpButton").SetActive(false);
 #else
-            dropdown.gameObject.SetActive(false);
-            inputField = GameObject.Find("InputField").GetComponent<InputField>();
+            // Monitor Display info
+            var winDispInfoList = AgoraNativeBridge.GetWinDisplayInfo();
+            if (winDispInfoList != null)
+            {
+                foreach (var dpInfo in winDispInfoList)
+                {
+                    WinDisplays.Add(dpInfo.MonitorInfo.monitor);
+                }
+            }
+
+            // Window ID info
+            Dictionary<string, System.IntPtr> winWinIdList;
+            AgoraNativeBridge.GetDesktopWindowHandlesAndTitles(out winWinIdList);
+            if (winWinIdList != null)
+            {
+                dropdown.options = (winWinIdList.Select(w =>
+                    new Dropdown.OptionData(string.Format("{0, -20} | {1}",
+                        w.Key.Substring(0, System.Math.Min(w.Key.Length, 20)), w.Value))).ToList());
+            }
 #endif
             WindowOptionDropdown = dropdown;
         }
@@ -75,18 +96,36 @@ public class DesktopScreenShare : PlayerViewControllerBase
         ScreenCaptureParameters sparams = new ScreenCaptureParameters
         {
             captureMouseCursor = true,
-            frameRate = 30
+            frameRate = 15
         };
 
         mRtcEngine.StopScreenCapture();
 
 #if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-        mRtcEngine.StartScreenCaptureByDisplayId(getDisplayId(displayID0or1), default(Rectangle), sparams);  // 
+        mRtcEngine.StartScreenCaptureByDisplayId(MacDisplays[CurrentDisplay], default(Rectangle), sparams); 
+        CurrentDisplay = (CurrentDisplay + 1) % MacDisplays.Count;
 #else
-        TestRectCrop(displayID0or1);
+        ShareWinDisplayScreen(CurrentDisplay);
+        CurrentDisplay = (CurrentDisplay + 1) % WinDisplays.Count;
 #endif
-        displayID0or1 = 1 - displayID0or1;
     }
+
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+    void ShareWinDisplayScreen(int index)
+    {
+        var screenRect = new Rectangle
+        {
+            x = WinDisplays[index].left,
+            y = WinDisplays[index].top,
+            width = WinDisplays[index].right - WinDisplays[index].left,
+            height = WinDisplays[index].bottom - WinDisplays[index].top
+        };
+        Debug.Log(string.Format(">>>>> Start sharing display {0}: {1} {2} {3} {4}", index, screenRect.x,
+            screenRect.y, screenRect.width, screenRect.height));
+        var ret = mRtcEngine.StartScreenCaptureByScreenRect(screenRect,
+            new Rectangle { x = 0, y = 0, width = 0, height = 0 }, default(ScreenCaptureParameters));
+    }
+#endif
 
     void TestRectCrop(int order)
     {
@@ -101,27 +140,8 @@ public class DesktopScreenShare : PlayerViewControllerBase
         if (rc != 0) Debug.LogWarning("rc = " + rc);
     }
 
-    uint getDisplayId(int k)
-    {
-#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-        List<uint> ids = AgoraNativeBridge.GetMacDisplayIds();
-#else
-        List<uint> ids = new List<uint>();
-#endif
-
-        if (k < ids.Count)
-        {
-            return ids[k];
-        }
-        return 0;
-    }
-
-    private InputField inputField;
-
     void OnShareWindowClick()
     {
-#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
-
         char[] delimiterChars = { '|' };
         if (WindowOptionDropdown == null) return;
         string option = WindowOptionDropdown.options[WindowOptionDropdown.value].text;
@@ -135,17 +155,5 @@ public class DesktopScreenShare : PlayerViewControllerBase
         mRtcEngine.StopScreenCapture();
 
         mRtcEngine.StartScreenCaptureByWindowId(int.Parse(wid), default(Rectangle), default(ScreenCaptureParameters));
-
-#elif UNITY_EDITOR_WIN ||  UNITY_STANDALONE_WIN
-
-        int winHandle;
-        if (int.TryParse(inputField.text, out winHandle))
-        {
-            mRtcEngine.StopScreenCapture();
-            mRtcEngine.StartScreenCaptureByWindowId(winHandle, default(Rectangle), default(ScreenCaptureParameters));
-        }
-
-#endif
-
     }
 }
