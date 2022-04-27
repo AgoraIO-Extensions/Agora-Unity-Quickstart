80f3ff0e5d0dc3127433ab1541137c3fe61e73a1
xiayangqun <444746024@qq.com>
2022-04-27 19:47:39 +0800

    Jira/ms 141758/dev/xiayangqun take snapshot (#65)
    * add API-Example: TakeSnapshot
    
    * fix error
    
    Co-authored-by: 夏羊群 <xiayangqun@agora.io>

---
+++ API-Example-Unity/Assets/API-Example/examples/advanced/TakeSnapshot/TakeSnapshot.cs
@@ -0,0 +1,288 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
+using agora.rtc;
+using agora.util;
+using UnityEditor;
+using Logger = agora.util.Logger;
+using System.IO;
+
+namespace Agora_Plugin.API_Example.examples.basic.TakeSnapshot
+{
+
+    public class TakeSnapshot : MonoBehaviour
+    {
+
+        [FormerlySerializedAs("AgoraBaseProfile")]
+        [SerializeField]
+        private AgoraBaseProfile agoraBaseProfile;
+
+        [Header("_____________Basic Configuration_____________")]
+        [FormerlySerializedAs("APP_ID")]
+        [SerializeField]
+        private string appID = "";
+
+        [FormerlySerializedAs("TOKEN")]
+        [SerializeField]
+        private string token = "";
+
+        [FormerlySerializedAs("CHANNEL_NAME")]
+        [SerializeField]
+        public string channelName = "";
+
+        public Text logText;
+        internal Logger Logger;
+        internal IAgoraRtcEngine _mRtcEngine = null;
+        private const float Offset = 100;
+        public uint localUid = 0;
+
+        // Use this for initialization
+        private void Start()
+        {
+            LoadAssetData();
+            CheckAppId();
+            SetupUI();
+            EnableUI(false);
+            InitEngine();
+            JoinChannel();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            PermissionHelper.RequestMicrophontPermission();
+            PermissionHelper.RequestCameraPermission();
+        }
+
+       
+        //Show data in AgoraBasicProfile
+        [ContextMenu("ShowAgoraBasicProfileData")]
+        public void LoadAssetData()
+        {
+            if (agoraBaseProfile == null) return;
+            appID = agoraBaseProfile.appID;
+            token = agoraBaseProfile.token;
+            channelName = agoraBaseProfile.channelName;
+        }
+
+
+        private void CheckAppId()
+        {
+            Logger = new Logger(logText);
+            Logger.DebugAssert(appID.Length > 10, "Please fill in your appId in VideoCanvas!!!!!");
+        }
+
+        private void InitEngine()
+        {
+            _mRtcEngine = AgoraRtcEngine.CreateAgoraRtcEngine();
+            UserEventHandler handler = new UserEventHandler(this);
+            RtcEngineContext context = new RtcEngineContext(null, appID, null, true,
+                                        CHANNEL_PROFILE_TYPE.CHANNEL_PROFILE_LIVE_BROADCASTING,
+                                        AUDIO_SCENARIO_TYPE.AUDIO_SCENARIO_DEFAULT);
+            _mRtcEngine.Initialize(context);
+            _mRtcEngine.InitEventHandler(handler);
+            _mRtcEngine.EnableAudio();
+            _mRtcEngine.EnableVideo();
+            _mRtcEngine.SetClientRole(CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER);
+        }
+
+        private void JoinChannel()
+        {
+            _mRtcEngine.JoinChannel(token, channelName);
+        }
+
+        private void SetupUI()
+        {
+            var but =this.transform.Find("TakeSnapshotButton").GetComponent<Button>();
+            but.onClick.AddListener(OnTakeSnapshotButtonPress);
+               
+        }
+
+        public void EnableUI(bool visible)
+        {
+            var button = this.transform.Find("TakeSnapshotButton").gameObject;
+            button.SetActive(visible);
+        }
+
+        private void OnTakeSnapshotButtonPress()
+        {
+            //uid 0 means self. you can get other user uid in OnUserJoined()
+            uint uid = 0;
+            string filePath =  Path.Combine(Application.persistentDataPath,"takeSnapshot.jpg");
+            int nRet = _mRtcEngine.TakeSnapshot(this.channelName, uid, filePath);
+            this.Logger.UpdateLog("TakeSnapshot nRet: " + nRet);
+            this.Logger.UpdateLog("TakeSnapshot in " + filePath);
+        }
+
+        private void OnApplicationQuit()
+        {
+            Debug.Log("OnApplicationQuit");
+            agoraBaseProfile.isHomeStart = false;
+            if (_mRtcEngine == null) return;
+            _mRtcEngine.LeaveChannel();
+            _mRtcEngine.Dispose();
+        }
+
+        internal static void MakeVideoView(uint uid, string channelId = "")
+        {
+            var go = GameObject.Find(uid.ToString());
+            if (!ReferenceEquals(go, null))
+            {
+                return; // reuse
+            }
+
+            // create a GameObject and assign to this new user
+            var videoSurface = MakeImageSurface(uid.ToString());
+            if (ReferenceEquals(videoSurface, null)) return;
+            // configure videoSurface
+            if (uid == 0)
+            {
+                videoSurface.SetForUser(uid, channelId);
+            }
+            else
+            {
+                videoSurface.SetForUser(uid, channelId, VIDEO_SOURCE_TYPE.VIDEO_SOURCE_REMOTE);
+            }
+            videoSurface.SetEnable(true);
+        }
+
+        // VIDEO TYPE 1: 3D Object
+        private static AgoraVideoSurface MakePlaneSurface(string goName)
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+            if (go == null)
+            {
+                return null;
+            }
+
+            go.name = goName;
+            // set up transform
+            go.transform.Rotate(-90.0f, 0.0f, 0.0f);
+            var yPos = Random.Range(3.0f, 5.0f);
+            var xPos = Random.Range(-2.0f, 2.0f);
+            go.transform.position = new Vector3(xPos, yPos, 0f);
+            go.transform.localScale = new Vector3(0.25f, 0.5f, 0.5f);
+
+            // configure videoSurface
+            var videoSurface = go.AddComponent<AgoraVideoSurface>();
+            return videoSurface;
+        }
+
+        // Video TYPE 2: RawImage
+        private static AgoraVideoSurface MakeImageSurface(string goName)
+        {
+            GameObject go = new GameObject();
+
+            if (go == null)
+            {
+                return null;
+            }
+
+            go.name = goName;
+            // to be renderered onto
+            go.AddComponent<RawImage>();
+            // make the object draggable
+            go.AddComponent<UIElementDrag>();
+            var canvas = GameObject.Find("VideoCanvas");
+            if (canvas != null)
+            {
+                go.transform.parent = canvas.transform;
+                Debug.Log("add video view");
+            }
+            else
+            {
+                Debug.Log("Canvas is null video view");
+            }
+
+            // set up transform
+            go.transform.Rotate(0f, 0.0f, 180.0f);
+            //var xPos = Random.Range(Offset - Screen.width / 2f, Screen.width / 2f - Offset);
+            //var yPos = Random.Range(Offset, Screen.height / 2f - Offset);
+            //Debug.Log("position x " + xPos + " y: " + yPos);
+            go.transform.localPosition = new Vector3(Screen.width / 2f - Offset, Screen.height / 2f - Offset, 0f);
+            go.transform.localScale = new Vector3(2f, 3f, 1f);
+
+            // configure videoSurface
+            var videoSurface = go.AddComponent<AgoraVideoSurface>();
+            return videoSurface;
+        }
+
+        internal static void DestroyVideoView(uint uid)
+        {
+            var go = GameObject.Find(uid.ToString());
+            if (!ReferenceEquals(go, null))
+            {
+                Destroy(go);
+            }
+        }
+    }
+
+    internal class UserEventHandler : IAgoraRtcEngineEventHandler
+    {
+        private readonly TakeSnapshot _takeSnapshot;
+
+        internal UserEventHandler(TakeSnapshot videoSample)
+        {
+            _takeSnapshot = videoSample;
+        }
+
+        public override void OnWarning(int warn, string msg)
+        {
+            _takeSnapshot.Logger.UpdateLog(string.Format("OnWarning warn: {0}, msg: {1}", warn, msg));
+        }
+
+        public override void OnError(int err, string msg)
+        {
+            _takeSnapshot.Logger.UpdateLog(string.Format("OnError err: {0}, msg: {1}", err, msg));
+        }
+
+        public override void OnJoinChannelSuccess(RtcConnection connection, int elapsed)
+        {
+            Debug.Log("Agora: OnJoinChannelSuccess ");
+            _takeSnapshot.Logger.UpdateLog(string.Format("sdk version: ${0}",
+                _takeSnapshot._mRtcEngine.GetVersion()));
+            _takeSnapshot.Logger.UpdateLog(
+                string.Format("OnJoinChannelSuccess channelName: {0}, uid: {1}, elapsed: {2}",
+                                connection.channelId, connection.localUid, elapsed));
+
+            _takeSnapshot.localUid = connection.localUid;
+            _takeSnapshot.EnableUI(true);
+            TakeSnapshot.MakeVideoView(0);
+        }
+
+        public override void OnRejoinChannelSuccess(RtcConnection connection, int elapsed)
+        {
+            _takeSnapshot.Logger.UpdateLog("OnRejoinChannelSuccess");
+        }
+
+        public override void OnLeaveChannel(RtcConnection connection, RtcStats stats)
+        {
+            _takeSnapshot.Logger.UpdateLog("OnLeaveChannel");
+            TakeSnapshot.DestroyVideoView(0);
+        }
+
+        public override void OnClientRoleChanged(RtcConnection connection, CLIENT_ROLE_TYPE oldRole, CLIENT_ROLE_TYPE newRole)
+        {
+            _takeSnapshot.Logger.UpdateLog("OnClientRoleChanged");
+        }
+
+        public override void OnUserJoined(RtcConnection connection, uint uid, int elapsed)
+        {
+            _takeSnapshot.Logger.UpdateLog(string.Format("OnUserJoined uid: ${0} elapsed: ${1}", uid, elapsed));
+            TakeSnapshot.MakeVideoView(uid, _takeSnapshot.channelName);
+            _takeSnapshot.EnableUI(true); 
+        }
+
+        public override void OnUserOffline(RtcConnection connection, uint uid, USER_OFFLINE_REASON_TYPE reason)
+        {
+            _takeSnapshot.Logger.UpdateLog(string.Format("OnUserOffLine uid: ${0}, reason: ${1}", uid,
+                (int)reason));
+            TakeSnapshot.DestroyVideoView(uid);
+        }
+
+       
+    }
+
+}
