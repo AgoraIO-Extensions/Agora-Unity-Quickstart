3529b3ec50a06b5b86bced508ea3a9fba5c9dbfe
夏羊群 <xiayangqun@agora.io>
2022-04-29 15:37:24 +0800

    add Build Scene

--- API-Example-Unity/Assets/API-Example/examples/basic/JoinChannelVideo/JoinChannelVideo.cs
+++ API-Example-Unity/Assets/API-Example/examples/basic/JoinChannelVideo/JoinChannelVideo.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Serialization;
-using UnityEngine.SceneManagement;
 using agora.rtc;
 using agora.util;
-using UnityEditor;
 using Logger = agora.util.Logger;
 
 
