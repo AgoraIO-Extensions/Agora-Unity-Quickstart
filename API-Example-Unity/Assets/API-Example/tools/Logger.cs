e09496777e5703d9c0500e29263eb7f884e69090
rickagora <rick@agora.io>
2022-05-20 10:36:51 -0700

    Update logger tool and README

--- API-Example-Unity/Assets/API-Example/tools/Logger.cs
+++ API-Example-Unity/Assets/API-Example/tools/Logger.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Logger {
+public class Logger
+{
     Text text;
 
-    public Logger(Text text) {
+    public Logger(Text text)
+    {
         this.text = text;
     }
 
-    public void UpdateLog(string logMessage) {
-        Debug.Log(logMessage);
+    public void UpdateLog(string logMessage, bool error = false)
+    {
+        if (error)
+        {
+            Debug.LogError(logMessage);
+        }
+        else
+        {
+            Debug.Log(logMessage);
+        }
+
         string srcLogMessage = text.text;
-        if (srcLogMessage.Length > 1000) {
+        if (srcLogMessage.Length > 1000)
+        {
             srcLogMessage = "";
         }
         srcLogMessage += "\r\n \r\n";
@@ -19,9 +31,11 @@ public class Logger {
         text.text = srcLogMessage;
     }
 
-    public bool DebugAssert(bool condition, string message) {
-        if (!condition) {
-            UpdateLog(message);
+    public bool DebugAssert(bool condition, string message)
+    {
+        if (!condition)
+        {
+            UpdateLog("<color=red>" + message + "</color>", error: true);
             return false;
         }
         Debug.Assert(condition, message);
