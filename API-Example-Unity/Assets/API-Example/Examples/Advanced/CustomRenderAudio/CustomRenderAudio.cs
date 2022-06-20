8b08b9a471ff72702d8fe34fc0419268e1c929a5
HugoChaan <47882165+HugoChaan@users.noreply.github.com>
2022-06-20 11:39:40 +0800

    Dev/3.8.201 (#82)
    * fix api for SetExternalVideoSource
    
    * fix onStreamMessage
    
    * content inspeact callback now show in screen log
    
    * fix object mirror
    
    * remove used api call
    
    * fix screenShare
    
    * optimization  customcaptureAudio
    
    * fix audioMixing play error
    
    * hide ContentInspectScene
    
    * fix error
    
    * fix screen share not effect in LocalVideoTranscoder
    
    * fix gif error in startLocalVideoTranscoder
    
    * add SDK verison Text
    
    * replace Start to Play
    
    * fix Thread Error in CustomCapture/RenderAudio Case
    
    * fix ThreadExit in CustomRenderAudio
    
    * optimize ChannelMediaPelay demo
    
    * optimize DualCamera demo
    
    * optimize MediaPlayer demo
    
    * optimize Metadata demo
    
    * optimize ScreenShareDemo
    
    * optimize StartLocalVideoTranscoder demo
    
    * optimize VoiceChangerScene demo
    
    * and image bkg in virtualVideoTRranscoder.cs
    
    * and local file play in mpk
    
    * Log StartRhythmPlayer.cs config changed
    
    * fix Log font size to 30
    
    * set Logger can show max word to 400
    
    * fix right ) error
    
    * add Logs-Viewer
    
    * fix font size
    
    * rename folders
    
    * rename folders
    
    * remove Api-example
    
    * rename Api-example
    
    * add ignore
    
    * add lost scrpit in homescene
    
    * modify Folder name in CommandBuild.cs
    
    Co-authored-by: CYG <cyg@CYGdeMacBook-Pro-2.local>
    Co-authored-by: 夏羊群 <xiayangqun@agora.io>

---
+++ API-Example-Unity/Assets/API-Example/Examples/Advanced/CustomRenderAudio/CustomRenderAudio.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using RingBuffer;
+using UnityEngine.Serialization;
+using agora.rtc;
+using agora.util;
+using System.Runtime.InteropServices;
+using Logger = agora.util.Logger;
+
+namespace CustomRenderAudio
+{
+    public class CustomRenderAudio : MonoBehaviour
+    {
+        [FormerlySerializedAs("appIdInput")]
+        [SerializeField]
+        private AppIdInput _appIdInput;
+
+        [Header("_____________Basic Configuration_____________")]
+        [FormerlySerializedAs("APP_ID")]
+        [SerializeField]
+        private string _appID = "";
+
+        [FormerlySerializedAs("TOKEN")]
+        [SerializeField]
+        private string _token = "";
+
+        [FormerlySerializedAs("CHANNEL_NAME")]
+        [SerializeField]
+        private string _channelName = "";
+
+        public Text LogText;
+        internal Logger Log;
+        internal IRtcEngine RtcEngine;
+
+
+        private const int CHANNEL = 1;
+        private const int SAMPLE_RATE = 44100;
+        private const int PULL_FREQ_PER_SEC = 100;
+
+
+        private RingBuffer<float> _audioBuffer;
+        private AudioClip _audioClip;
+
+
+        private Thread _pullAudioFrameThread;
+        private System.Object _pullAudioFrameThreadSignal = new System.Object();
+
+        private int _writeCount;
+        private int _readCount;
+
+        private void Start()
+        {
+            LoadAssetData();
+            if (CheckAppId())
+            {
+                InitRtcEngine();
+                JoinChannel();
+                var aud = InitAudioSource();
+                StartPullAudioFrame(aud, "externalClip");
+            }
+        }
+
+        private void Update()
+        {
+            PermissionHelper.RequestMicrophontPermission();
+        }
+
+        private bool CheckAppId()
+        {
+            Log = new Logger(LogText);
+            return Log.DebugAssert(_appID.Length > 10, "Please fill in your appId in API-Example/profile/appIdInput.asset");
+        }
+
+        //Show data in AgoraBasicProfile
+        [ContextMenu("ShowAgoraBasicProfileData")]
+        private void LoadAssetData()
+        {
+            if (_appIdInput == null) return;
+            _appID = _appIdInput.appID;
+            _token = _appIdInput.token;
+            _channelName = _appIdInput.channelName;
+        }
+
+        private void InitRtcEngine()
+        {
+            RtcEngine = agora.rtc.RtcEngine.CreateAgoraRtcEngine();
+            UserEventHandler handler = new UserEventHandler(this);
+            //be care, enableAudioDevice need be false
+            RtcEngineContext context = new RtcEngineContext(_appID, 0, false,
+                                        CHANNEL_PROFILE_TYPE.CHANNEL_PROFILE_LIVE_BROADCASTING,
+                                        AUDIO_SCENARIO_TYPE.AUDIO_SCENARIO_DEFAULT);
+            var ret = RtcEngine.Initialize(context);
+            RtcEngine.InitEventHandler(handler);
+        }
+
+        private void JoinChannel()
+        {
+            RtcEngine.EnableAudio();
+            var nRet = RtcEngine.SetExternalAudioSink(SAMPLE_RATE, CHANNEL);
+            this.Log.UpdateLog("SetExternalAudioSink ret:" + nRet);
+            RtcEngine.JoinChannel(_token, _channelName);
+        }
+
+        private AudioSource InitAudioSource()
+        {
+            var aud = GetComponent<AudioSource>();
+            if (aud == null)
+            {
+                aud = gameObject.AddComponent<AudioSource>();
+            }
+            return aud;
+        }
+
+        private void StartPullAudioFrame(AudioSource aud, string clipName)
+        {
+
+            // 1-sec-length buffer
+            var bufferLength = SAMPLE_RATE * CHANNEL;
+            _audioBuffer = new RingBuffer<float>(bufferLength, true);
+
+            _pullAudioFrameThread = new Thread(PullAudioFrameThread);
+            _pullAudioFrameThread.Start();
+
+            _audioClip = AudioClip.Create(clipName,
+                SAMPLE_RATE / PULL_FREQ_PER_SEC * CHANNEL, CHANNEL, SAMPLE_RATE, true,
+                OnAudioRead);
+            aud.clip = _audioClip;
+            aud.loop = true;
+            aud.Play();
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("OnDestroy");
+            lock (_pullAudioFrameThreadSignal)
+            {
+                if (RtcEngine == null) return;
+                RtcEngine.InitEventHandler(null);
+                RtcEngine.LeaveChannel();
+                RtcEngine.Dispose();
+                RtcEngine = null;
+            }
+        }
+
+
+        private void PullAudioFrameThread()
+        {
+            var avsync_type = 0;
+            var bytesPerSample = 2;
+            var type = AUDIO_FRAME_TYPE.FRAME_TYPE_PCM16;
+            var channels = CHANNEL;
+            var samples = SAMPLE_RATE / PULL_FREQ_PER_SEC * CHANNEL;
+            var samplesPerSec = SAMPLE_RATE;
+            var buffer = new byte[samples * bytesPerSample];
+            var freq = 1000 / PULL_FREQ_PER_SEC;
+
+            var tic = new TimeSpan(DateTime.Now.Ticks);
+
+            AudioFrame audioFrame = new AudioFrame(type, samples, BYTES_PER_SAMPLE.TWO_BYTES_PER_SAMPLE, channels, samplesPerSec, buffer, 0, avsync_type);
+            IntPtr audioFrameBuffer = Marshal.AllocHGlobal(samples * bytesPerSample * channels);
+            audioFrame.buffer = (UInt64)audioFrameBuffer;
+            audioFrame.bufferPtr = audioFrameBuffer;
+
+            while (true)
+            {
+                lock (_pullAudioFrameThreadSignal)
+                {
+                    if (RtcEngine == null)
+                    {
+                        break;
+                    }
+
+                    var toc = new TimeSpan(DateTime.Now.Ticks);
+                    if (toc.Subtract(tic).Duration().Milliseconds >= freq)
+                    {
+                        tic = new TimeSpan(DateTime.Now.Ticks);
+                        var ret = RtcEngine.PullAudioFrame(audioFrame);
+
+                        Debug.Log("PullAudioFrame returns: " + ret);
+
+                        if (ret == 0)
+                        {
+                            Marshal.Copy((IntPtr)audioFrame.buffer, audioFrame.RawBuffer, 0, audioFrame.RawBuffer.Length);
+                            var floatArray = ConvertByteToFloat16(audioFrame.RawBuffer);
+                            lock (_audioBuffer)
+                            {
+                                _audioBuffer.Put(floatArray);
+                            }
+
+                            _writeCount += floatArray.Length;
+                        }
+                    }
+                }
+                Thread.Sleep(1);
+            }
+
+            Marshal.FreeHGlobal(audioFrameBuffer);
+        }
+
+        private static float[] ConvertByteToFloat16(byte[] byteArray)
+        {
+            var floatArray = new float[byteArray.Length / 2];
+            for (var i = 0; i < floatArray.Length; i++)
+            {
+                floatArray[i] = BitConverter.ToInt16(byteArray, i * 2) / 32768f; // -Int16.MinValue
+            }
+
+            return floatArray;
+        }
+
+
+        private void OnAudioRead(float[] data)
+        {
+            //if (!_startSignal) return;
+            for (var i = 0; i < data.Length; i++)
+            {
+                lock (_audioBuffer)
+                {
+                    if (_audioBuffer.Count > 0)
+                    {
+                        data[i] = _audioBuffer.Get();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                //readCount += 1;
+            }
+
+            Debug.LogFormat("buffer length remains: {0}", _writeCount - _readCount);
+        }
+    }
+
+    internal class UserEventHandler : IRtcEngineEventHandler
+    {
+        private readonly CustomRenderAudio _customAudioSinkSample;
+
+        internal UserEventHandler(CustomRenderAudio customAudioSinkSample)
+        {
+            _customAudioSinkSample = customAudioSinkSample;
+        }
+
+        public override void OnJoinChannelSuccess(RtcConnection connection, int elapsed)
+        {
+            _customAudioSinkSample.Log.UpdateLog(string.Format("sdk version: {0}", _customAudioSinkSample.RtcEngine.GetVersion()));
+            _customAudioSinkSample.Log.UpdateLog(string.Format("onJoinChannelSuccess channelName: {0}, uid: {1}, elapsed: {2}", connection.channelId,
+                connection.localUid, elapsed));
+        }
+
+        public override void OnLeaveChannel(RtcConnection connection, RtcStats stats)
+        {
+            _customAudioSinkSample.Log.UpdateLog("OnLeaveChannelSuccess");
+        }
+
+        public override void OnWarning(int warn, string msg)
+        {
+            _customAudioSinkSample.Log.UpdateLog(string.Format("OnSDKWarning warn: {0}, msg: {1}", warn, msg));
+        }
+
+        public override void OnError(int error, string msg)
+        {
+            _customAudioSinkSample.Log.UpdateLog(string.Format("OnSDKError error: {0}, msg: {1}", error, msg));
+        }
+
+        public override void OnConnectionLost(RtcConnection connection)
+        {
+            _customAudioSinkSample.Log.UpdateLog(string.Format("OnConnectionLost "));
+        }
+    }
+}
---
+++ API-Example-Unity/Assets/API-Example/Examples/Advanced/ScreenShare/ScreenShare.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using agora.rtc;
+using agora.util;
+using UnityEngine.Serialization;
+using Logger = agora.util.Logger;
+
+namespace Agora_Plugin.API_Example.examples.advanced.ScreenShare
+{
+    public class ScreenShare : MonoBehaviour
+    {
+        [FormerlySerializedAs("appIdInput")]
+        [SerializeField]
+        private AppIdInput _appIdInput;
+
+        [Header("_____________Basic Configuration_____________")]
+        [FormerlySerializedAs("APP_ID")]
+        [SerializeField]
+        private string _appID = "";
+
+        [FormerlySerializedAs("TOKEN")]
+        [SerializeField]
+        private string _token = "";
+
+        [FormerlySerializedAs("CHANNEL_NAME")]
+        [SerializeField]
+        private string _channelName = "";
+
+        public Text LogText;
+        internal Logger Log;
+        internal IRtcEngine RtcEngine = null;
+
+        private Dropdown _winIdSelect;
+        private Button _startShareBtn;
+        private Button _stopShareBtn;
+
+        // Use this for initialization
+        private void Start()
+        {
+#if UNITY_IPHONE || UNITY_ANDROID
+            this.LogText.text = "ios or Android is not supported, but you could see how it works on the Editor for Windows/MacOS";
+#else
+            LoadAssetData();
+            if (CheckAppId())
+            {
+                InitEngine();
+                PrepareScreenCapture();
+                JoinChannel();
+            }
+#endif
+        }
+
+        private bool CheckAppId()
+        {
+            Log = new Logger(LogText);
+            return Log.DebugAssert(_appID.Length > 10, "Please fill in your appId in API-Example/profile/appIdInput.asset");
+        }
+
+        //Show data in AgoraBasicProfile
+        [ContextMenu("ShowAgoraBasicProfileData")]
+        private void LoadAssetData()
+        {
+            if (_appIdInput == null) return;
+            _appID = _appIdInput.appID;
+            _token = _appIdInput.token;
+            _channelName = _appIdInput.channelName;
+        }
+
+        private void JoinChannel()
+        {
+            RtcEngine.EnableAudio();
+            RtcEngine.EnableVideo();
+            RtcEngine.SetClientRole(CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER);
+            
+            ChannelMediaOptions options = new ChannelMediaOptions();
+            options.autoSubscribeAudio.SetValue(true);
+            options.autoSubscribeVideo.SetValue(true);
+            options.publishAudioTrack.SetValue(true);
+            options.publishCameraTrack.SetValue(false);
+            options.publishScreenTrack.SetValue(true);
+            options.enableAudioRecordingOrPlayout.SetValue(true);
+            options.clientRoleType.SetValue(CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER);
+            var ret = RtcEngine.JoinChannel(_token, _channelName, 0, options);
+            Debug.Log("JoinChannel returns: " + ret);
+        }
+
+        private void InitEngine()
+        {
+            RtcEngine = agora.rtc.RtcEngine.CreateAgoraRtcEngine();
+            UserEventHandler handler = new UserEventHandler(this);
+            RtcEngineContext context = new RtcEngineContext(_appID, 0, true,
+                                        CHANNEL_PROFILE_TYPE.CHANNEL_PROFILE_LIVE_BROADCASTING,
+                                        AUDIO_SCENARIO_TYPE.AUDIO_SCENARIO_DEFAULT);
+            RtcEngine.Initialize(context);
+            RtcEngine.InitEventHandler(new UserEventHandler(this));
+        }
+
+        private void PrepareScreenCapture()
+        {
+            _winIdSelect = GameObject.Find("winIdSelect").GetComponent<Dropdown>();
+
+            if (_winIdSelect == null || RtcEngine == null) return;
+
+            _winIdSelect.ClearOptions();
+
+            SIZE t = new SIZE();
+            t.width = 360;
+            t.height = 240;
+            SIZE s = new SIZE();
+            s.width = 360;
+            s.height = 240;
+            var info = RtcEngine.GetScreenCaptureSources(t, s, true);
+
+            _winIdSelect.AddOptions(info.Select(w =>
+                    new Dropdown.OptionData(
+                        string.Format("{0}: {1}-{2} | {3}",w.type, w.sourceName, w.sourceTitle, w.sourceId)))
+                .ToList());
+            _startShareBtn = GameObject.Find("startShareBtn").GetComponent<Button>();
+            _stopShareBtn = GameObject.Find("stopShareBtn").GetComponent<Button>();
+            if (_startShareBtn != null) _startShareBtn.onClick.AddListener(OnStartShareBtnClick);
+            if (_stopShareBtn != null)
+            {
+                _stopShareBtn.onClick.AddListener(OnStopShareBtnClick);
+                _stopShareBtn.gameObject.SetActive(false);
+            }
+        }
+
+        private void OnStartShareBtnClick()
+        {
+            if (RtcEngine == null) return;
+
+            if (_startShareBtn != null) _startShareBtn.gameObject.SetActive(false);
+            if (_stopShareBtn != null) _stopShareBtn.gameObject.SetActive(true);
+            RtcEngine.StopScreenCapture();
+
+            if (_winIdSelect == null) return;
+            var option = _winIdSelect.options[_winIdSelect.value].text;
+            if (string.IsNullOrEmpty(option)) return;
+            if (option.Contains("ScreenCaptureSourceType_Window"))
+            {
+                var windowId = option.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[1];
+                Log.UpdateLog(string.Format(">>>>> Start sharing {0}", windowId));
+                RtcEngine.StartScreenCaptureByWindowId(ulong.Parse(windowId), default(Rectangle),
+                        default(ScreenCaptureParameters));
+            }
+            else
+            {
+                var dispId = uint.Parse(option.Split("|".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)[1]);
+                Log.UpdateLog(string.Format(">>>>> Start sharing display {0}", dispId));
+                RtcEngine.StartScreenCaptureByDisplayId(dispId, default(Rectangle),
+                    new ScreenCaptureParameters { captureMouseCursor = true, frameRate = 30 });
+            }
+        }
+
+        private void OnStopShareBtnClick()
+        {
+            if (_startShareBtn != null) _startShareBtn.gameObject.SetActive(true);
+            if (_stopShareBtn != null) _stopShareBtn.gameObject.SetActive(false);
+            RtcEngine.StopScreenCapture();
+        }
+
+        private void OnDestroy()
+        {
+            Debug.Log("OnDestroy");
+            if (RtcEngine == null) return;
+            RtcEngine.InitEventHandler(null);
+            RtcEngine.LeaveChannel();
+            RtcEngine.Dispose();
+        }
+
+      
+        internal string GetChannelName()
+        {
+            return _channelName;
+        }
+
+        internal static void DestroyVideoView(uint uid)
+        {
+            var go = GameObject.Find(uid.ToString());
+            if (!ReferenceEquals(go, null))
+            {
+                Destroy(go);
+            }
+        }
+
+        internal static void MakeVideoView(uint uid, string channelId = "", VIDEO_SOURCE_TYPE videoSourceType = VIDEO_SOURCE_TYPE.VIDEO_SOURCE_CAMERA)
+        {
+            var go = GameObject.Find(uid.ToString());
+            if (!ReferenceEquals(go, null))
+            {
+                return; // reuse
+            }
+
+            // create a GameObject and assign to this new user
+            var videoSurface = MakeImageSurface(uid.ToString());
+            if (ReferenceEquals(videoSurface, null)) return;
+            // configure videoSurface
+            videoSurface.SetForUser(uid, channelId, videoSourceType);
+            videoSurface.SetEnable(true);
+
+            videoSurface.OnTextureSizeModify += (int width, int height) =>
+            {
+                float scale = (float)height / (float)width;
+                videoSurface.transform.localScale = new Vector3(-5, 5 * scale, 1);
+                Debug.Log("OnTextureSizeModify: " + width + "  " + height);
+            };
+        }
+
+        // VIDEO TYPE 1: 3D Object
+        private static VideoSurface MakePlaneSurface(string goName)
+        {
+            var go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+
+            if (go == null)
+            {
+                return null;
+            }
+
+            go.name = goName;
+            // set up transform
+            go.transform.Rotate(-90.0f, 0.0f, 0.0f);
+            go.transform.position = Vector3.zero;
+            go.transform.localScale = new Vector3(0.25f, 0.5f, .5f);
+
+            // configure videoSurface
+            var videoSurface = go.AddComponent<VideoSurface>();
+            return videoSurface;
+        }
+
+        // Video TYPE 2: RawImage
+        private static VideoSurface MakeImageSurface(string goName)
+        {
+            var go = new GameObject();
+
+            if (go == null)
+            {
+                return null;
+            }
+
+            go.name = goName;
+            // to be renderered onto
+            go.AddComponent<RawImage>();
+            // make the object draggable
+            go.AddComponent<UIElementDrag>();
+            var canvas = GameObject.Find("VideoCanvas");
+            if (canvas != null)
+            {
+                go.transform.parent = canvas.transform;
+                Debug.Log("add video view");
+            }
+            else
+            {
+                Debug.Log("Canvas is null video view");
+            }
+
+            // set up transform
+            go.transform.Rotate(0f, 0.0f, 180.0f);
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localScale = new Vector3(3f, 4f, 1f);
+
+            // configure videoSurface
+            var videoSurface = go.AddComponent<VideoSurface>();
+            return videoSurface;
+        }
+    }
+
+    internal class UserEventHandler : IRtcEngineEventHandler
+    {
+        private readonly ScreenShare _desktopScreenShare;
+
+        internal UserEventHandler(ScreenShare desktopScreenShare)
+        {
+            _desktopScreenShare = desktopScreenShare;
+        }
+
+        public override void OnWarning(int warn, string msg)
+        {
+            _desktopScreenShare.Log.UpdateLog(string.Format("OnWarning warn: {0}, msg: {1}", warn, msg));
+        }
+
+        public override void OnError(int err, string msg)
+        {
+            _desktopScreenShare.Log.UpdateLog(string.Format("OnError err: {0}, msg: {1}", err, msg));
+        }
+
+        public override void OnJoinChannelSuccess(RtcConnection connection, int elapsed)
+        {
+            _desktopScreenShare.Log.UpdateLog(string.Format("sdk version: ${0}",
+                _desktopScreenShare.RtcEngine.GetVersion()));
+            _desktopScreenShare.Log.UpdateLog(
+                string.Format("OnJoinChannelSuccess channelName: {0}, uid: {1}, elapsed: {2}",
+                                connection.channelId, connection.localUid, elapsed));
+            ScreenShare.MakeVideoView(0, "", VIDEO_SOURCE_TYPE.VIDEO_SOURCE_SCREEN);
+        }
+
+        public override void OnRejoinChannelSuccess(RtcConnection connection, int elapsed)
+        {
+            _desktopScreenShare.Log.UpdateLog("OnRejoinChannelSuccess");
+        }
+
+        public override void OnLeaveChannel(RtcConnection connection, RtcStats stats)
+        {
+            _desktopScreenShare.Log.UpdateLog("OnLeaveChannel");
+            ScreenShare.DestroyVideoView(connection.localUid);
+            ScreenShare.DestroyVideoView(0);
+        }
+
+        public override void OnClientRoleChanged(RtcConnection connection, CLIENT_ROLE_TYPE oldRole, CLIENT_ROLE_TYPE newRole)
+        {
+            _desktopScreenShare.Log.UpdateLog("OnClientRoleChanged");
+        }
+
+        public override void OnUserJoined(RtcConnection connection, uint uid, int elapsed)
+        {
+            _desktopScreenShare.Log.UpdateLog(string.Format("OnUserJoined uid: ${0} elapsed: ${1}", uid, elapsed));
+            ScreenShare.MakeVideoView(uid, _desktopScreenShare.GetChannelName(), VIDEO_SOURCE_TYPE.VIDEO_SOURCE_REMOTE);
+        }
+
+        public override void OnUserOffline(RtcConnection connection, uint uid, USER_OFFLINE_REASON_TYPE reason)
+        {
+            _desktopScreenShare.Log.UpdateLog(string.Format("OnUserOffLine uid: ${0}, reason: ${1}", uid,
+                (int)reason));
+            ScreenShare.DestroyVideoView(uid);
+        }
+    }
+}
