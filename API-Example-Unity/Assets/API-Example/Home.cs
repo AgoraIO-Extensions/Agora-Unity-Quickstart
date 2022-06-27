e3c98a41eb978f6434ebfa86490648530d6a90fe
夏羊群 <xiayangqun@agora.io>
2022-06-27 17:35:46 +0800

    add build scene

--- API-Example-Unity/Assets/API-Example/Home.cs
+++ API-Example-Unity/Assets/API-Example/Home.cs
@@ -51,6 +51,7 @@ public class Home : MonoBehaviour
         "SetVideoEncodeConfigurationScene",
         "StartLocalVideoTranscoderScene",
         "SpatialAudioWithMediaPlayerScene",
+        "StartDirectCdnStreamingScene",
         "StartRhythmPlayerScene",
         "StartRtmpStreamWithTranscodingScene",
         "StreamMessageScene",
