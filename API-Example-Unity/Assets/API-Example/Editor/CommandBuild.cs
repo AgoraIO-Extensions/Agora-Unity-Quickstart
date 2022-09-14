e75cd7fe66e6c9d0649681178fbdae1846d70c55
xiayangqun <444746024@qq.com>
2022-09-14 16:38:17 +0800

    Release/4.0.0 (#108)
    * add branch jump in main (#105)
    
    Co-authored-by: CYG <cyg@CYGdeMacBook-Pro-2.local>
    
    * list scene name (#107)
    
    Co-authored-by: 夏羊群 <xiayangqun@agora.io>
    
    Co-authored-by: HugoChaan <47882165+HugoChaan@users.noreply.github.com>
    Co-authored-by: CYG <cyg@CYGdeMacBook-Pro-2.local>
    Co-authored-by: 夏羊群 <xiayangqun@agora.io>

--- API-Example-Unity/Assets/API-Example/Editor/CommandBuild.cs
+++ API-Example-Unity/Assets/API-Example/Editor/CommandBuild.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEditor.Build;
 #if UNITY_2018_4_OR_NEWER
@@ -13,45 +14,22 @@ public class CommandBuild : MonoBehaviour
 
     private static string[] GetAllScenes()
     {
-        string[] scenes = new string[] {
-            "Assets/API-Example/HomeScene.unity",
-            "Assets/API-Example/Examples/Basic/JoinChannelVideo/BasicVideoCallScene.unity",
-            "Assets/API-Example/Examples/Basic/JoinChannelAudio/BasicAudioCallScene.unity",
 
-            "Assets/API-Example/Examples/Advanced/AudioMixing/AudioMixingScene.unity",
-            "Assets/API-Example/Examples/Advanced/AudioSpectrum/AudioSpectrumScene.unity",
-            "Assets/API-Example/Examples/Advanced/ChannelMediaRelay/ChannelMediaRelayScene.unity",
-            "Assets/API-Example/Examples/Advanced/ContentInspect/ContentInspectScene.unity",
-            "Assets/API-Example/Examples/Advanced/CustomCaptureAudio/CustomCaptureAudioScene.unity",
-            "Assets/API-Example/Examples/Advanced/CustomCaptureVideo/CustomCaptureVideoScene.unity",
-            "Assets/API-Example/Examples/Advanced/CustomRenderAudio/CustomRenderAudioScene.unity",
-            "Assets/API-Example/Examples/Advanced/DeviceManager/DeviceManagerScene.unity",
-            "Assets/API-Example/Examples/Advanced/DualCamera/DualCameraScene.unity",
-            "Assets/API-Example/Examples/Advanced/JoinChannelVideoToken/JoinChannelVideoTokenScene.unity",
-            "Assets/API-Example/Examples/Advanced/JoinChannelWithUserAccount/JoinChannelWithUserAccountScene.unity",
-            "Assets/API-Example/Examples/Advanced/MediaPlayer/MediaPlayerScene.unity",
-            "Assets/API-Example/Examples/Advanced/MediaRecorder/MediaRecorderScene.unity",
-            "Assets/API-Example/Examples/Advanced/Metadata/MetadataScene.unity",
-            "Assets/API-Example/Examples/Advanced/ProcessAudioRawData/ProcessAudioRawDataScene.unity",
-            "Assets/API-Example/Examples/Advanced/ProcessVideoRawData/ProcessVideoRawDataScene.unity",
-            "Assets/API-Example/Examples/Advanced/PushEncodedVideoImage/PushEncodedVideoImageScene.unity",
-            "Assets/API-Example/Examples/Advanced/RtmpStreaming/RtmpStreamingScene.unity",
-            "Assets/API-Example/Examples/Advanced/ScreenShare/ScreenShareScene.unity",
-            "Assets/API-Example/Examples/Advanced/ScreenShareWhileVideoCall/ScreenShareWhileVideoCallScene.unity",
-            "Assets/API-Example/Examples/Advanced/SetBeautyEffectOptions/SetBeautyEffectOptionsScene.unity",
-            "Assets/API-Example/Examples/Advanced/SetEncryption/SetEncryptionScene.unity",
-            "Assets/API-Example/Examples/Advanced/SetVideoEncodeConfiguration/SetVideoEncodeConfigurationScene.unity",
-            "Assets/API-Example/Examples/Advanced/SpatialAudioWithMediaPlayer/SpatialAudioWithMediaPlayerScene.unity",
-            "Assets/API-Example/Examples/Advanced/StartDirectCdnStreaming/StartDirectCdnStreamingScene.unity",
-            "Assets/API-Example/Examples/Advanced/StartLocalVideoTranscoder/StartLocalVideoTranscoderScene.unity",
-            "Assets/API-Example/Examples/Advanced/StartRhythmPlayer/StartRhythmPlayerScene.unity",
-            "Assets/API-Example/Examples/Advanced/StartRtmpStreamWithTranscoding/StartRtmpStreamWithTranscodingScene.unity",
-            "Assets/API-Example/Examples/Advanced/StreamMessage/StreamMessageScene.unity",
-            "Assets/API-Example/Examples/Advanced/TakeSnapshot/TakeSnapshotScene.unity",
-            "Assets/API-Example/Examples/Advanced/VirtualBackground/VirtualBackgroundScene.unity",
-            "Assets/API-Example/Examples/Advanced/VoiceChanger/VoiceChangerScene.unity"
-        };
-        return scenes;
+        List<string> scenesList = new List<string>();
+        scenesList.Add("Assets/API-Example/HomeScene.unity");
+
+        string[] resFiles = AssetDatabase.FindAssets("t:Scene", new string[] { "Assets" });
+        for (int i = 0; i < resFiles.Length; i++)
+        {
+            resFiles[i] = AssetDatabase.GUIDToAssetPath(resFiles[i]);
+            Debug.Log(resFiles[i]);
+            if (resFiles[i] != "Assets/API-Example/HomeScene.unity")
+            {
+                scenesList.Add(resFiles[i]);
+            }
+        }
+
+        return scenesList.ToArray();
     }
 
     [MenuItem("Build/Android")]
