664a5dd3433543cdc0b8366b69ff63f9311eee79
夏羊群 <xiayangqun@agora.io>
2023-05-24 14:35:28 +0800

    fix error fun name

--- package_android_in_local.sh
+++ package_android_in_local.sh
@@ -1,4 +1,4 @@
-/Applications/Unity/Hub/Editor/2020.3.30f1c1/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographics -projectPath "./API-Example-Unity" -executeMethod Agora_RTC_Plugin.API_Example.CommandBuild.BuildAndrod
+/Applications/Unity/Hub/Editor/2020.3.30f1c1/Unity.app/Contents/MacOS/Unity -quit -batchmode -nographics -projectPath "./API-Example-Unity" -executeMethod Agora_RTC_Plugin.API_Example.CommandBuild.BuildAndroid
 cp -r ./android_studio_template/. ./Build/android_studio
 cd ./Build/android_studio
 ./gradlew assembleRelease
