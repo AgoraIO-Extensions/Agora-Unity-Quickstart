64603697efceae784832fa2fc3bc4d9ccce859e3
xiayangqun <444746024@qq.com>
2023-08-04 11:12:31 +0800

    fix ci android package (#199)
    * fix ci with android_build
    
    * fix error path
    
    * fix ci error
    
    * fix ci error
    
    * fix ci read suffix
    
    ---------
    
    Co-authored-by: 夏羊群 <xiayangqun@agora.io>

--- ci/build/remove_example_by_macor.py
+++ ci/build/remove_example_by_macor.py
@@ -1,15 +1,18 @@
 import sys
 import os
-import re 
+import re
 import shutil
 
 # xxxx/xxxx/Assets
 assets_root = sys.argv[1]
 RTC = sys.argv[2]
 RTM = sys.argv[3]
+android_studio_temple = os.path.join(
+    assets_root, "../../android_studio_template")
 
+print('remove example by macor.py {0},{1},{2},{3}'.format(
+    assets_root, RTC, RTM, android_studio_temple))
 
-print('remove example by macor.py {0},{1},{2}'.format(assets_root,RTC,RTM))
 
 def get_all_files(target_dir):
     files = []
@@ -22,19 +25,20 @@ def get_all_files(target_dir):
             files.append(each_file)
     return files
 
-def remove_key_word_in_path(file_path, key_word):
+
+def remove_key_word_in_path(file_path, key_word, suffix = ".cs"):
     files = get_all_files(file_path)
     for i in range(0, len(files)):
         file_name = files[i]
-        if file_name.endswith(".cs"):
-            # print(file_name)
-            f = open(file_name,'r',encoding='UTF-8')
-            content = f.read()
-            content = content.replace(key_word,'')
-            f.close()
-            f = open(file_name,'w')
-            f.write(content)
-            f.close()
+        if file_name.endswith(suffix) == False:
+            continue
+        f = open(file_name, 'r', encoding='UTF-8')
+        content = f.read()
+        f.close()
+        content = content.replace(key_word, '')
+        f = open(file_name, 'w')
+        f.write(content)
+        f.close()
 
 
 if RTC == 'false':
@@ -51,3 +55,7 @@ if RTC == 'false':
 
 if RTM == 'false':
     remove_key_word_in_path(assets_root, '#define AGORA_RTM')
+
+if RTC == 'false':
+    remove_key_word_in_path(os.path.join(android_studio_temple, "launcher"),
+                            "implementation files('../unityLibrary/libs/AgoraScreenShareExtension.aar')",".gradle")
