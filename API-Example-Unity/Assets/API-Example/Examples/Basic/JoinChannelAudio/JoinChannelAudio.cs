a0a65cc237409450cb52ea2446cc8fd1e07b26ea
夏羊群 <xiayangqun@agora.io>
2024-05-22 16:02:10 +0800

    add audio call quality panel

--- API-Example-Unity/Assets/API-Example/Examples/Basic/JoinChannelAudio/JoinChannelAudio.cs
+++ API-Example-Unity/Assets/API-Example/Examples/Basic/JoinChannelAudio/JoinChannelAudio.cs
@@ -36,6 +36,8 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
         private DeviceInfo[] _audioPlaybackDeviceInfos;
         public Dropdown _audioDeviceSelect;
         public Dropdown _areaSelect;
+        public RectTransform _qualityPanel;
+        public GameObject _qualityItemPrefab;
 
         // Start is called before the first frame update
         private void Start()
@@ -79,13 +81,16 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
             _channelName = _appIdInput.channelName;
         }
 
-        private void PrepareAreaList() {
+        private void PrepareAreaList()
+        {
             int index = 0;
             var areaList = new List<Dropdown.OptionData>();
             var enumNames = Enum.GetNames(typeof(AREA_CODE));
-            foreach (var name in enumNames) {
+            foreach (var name in enumNames)
+            {
                 areaList.Add(new Dropdown.OptionData(name));
-                if (name == "AREA_CODE_GLOB") {
+                if (name == "AREA_CODE_GLOB")
+                {
                     index = areaList.Count - 1;
                 }
             }
@@ -97,7 +102,7 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
         #region -- Button Events ---
         public void InitRtcEngine()
         {
-            var text =this._areaSelect.captionText.text;
+            var text = this._areaSelect.captionText.text;
             AREA_CODE areaCode = (AREA_CODE)Enum.Parse(typeof(AREA_CODE), text);
             this.Log.UpdateLog("Select AREA_CODE : " + areaCode);
 
@@ -116,6 +121,7 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
             RtcEngine.EnableAudio();
             RtcEngine.SetChannelProfile(CHANNEL_PROFILE_TYPE.CHANNEL_PROFILE_COMMUNICATION);
             RtcEngine.SetClientRole(CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER);
+            RtcEngine.EnableAudioVolumeIndication(200, 3, true);
         }
 
 
@@ -201,6 +207,92 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
             RtcEngine.LeaveChannel();
             RtcEngine.Dispose();
         }
+
+
+        public LocalAudioCallQualityPanel GetLocalAudioCallQualityPanel()
+        {
+            var panels = this._qualityPanel.GetComponentsInChildren<LocalAudioCallQualityPanel>();
+            if (panels != null && panels.Length > 0)
+            {
+                return panels[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool CreateLocalAudioCallQualityPanel()
+        {
+            if (GetLocalAudioCallQualityPanel() == null)
+            {
+                GameObject item = GameObject.Instantiate(this._qualityItemPrefab, this._qualityPanel);
+                item.AddComponent<LocalAudioCallQualityPanel>();
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DestroyLocalAudioCallQualityPanel()
+        {
+            var panel = GetLocalAudioCallQualityPanel();
+            if (panel)
+            {
+                GameObject.Destroy(panel.gameObject);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public RemoteAudioCallQualityPanel GetRemoteAudioCallQualityPanel(uint uid)
+        {
+            var panels = this._qualityPanel.GetComponentsInChildren<RemoteAudioCallQualityPanel>();
+            foreach (var panel in panels)
+            {
+                if (panel.Uid == uid)
+                {
+                    return panel;
+                }
+            }
+
+            return null;
+        }
+
+        public bool CreateRemoteAudioCallQualityPanel(uint uid)
+        {
+            if (GetRemoteAudioCallQualityPanel(uid) == null)
+            {
+                GameObject item = GameObject.Instantiate(this._qualityItemPrefab, this._qualityPanel);
+                var panel = item.AddComponent<RemoteAudioCallQualityPanel>();
+                panel.Uid = uid;
+                return true;
+
+            }
+            return false;
+        }
+
+        public bool DestroyRemoteAudioCallQualityPanel(uint uid)
+        {
+            var panel = GetRemoteAudioCallQualityPanel(uid);
+            if (panel)
+            {
+                GameObject.Destroy(panel.gameObject);
+                return true;
+            }
+            return false;
+        }
+
+        public void ClearAudioCallQualityPanel()
+        {
+            foreach (Transform child in this._qualityPanel)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
     }
 
     #region -- Agora Event ---
@@ -227,6 +319,7 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
             _audioSample.Log.UpdateLog(
                 string.Format("OnJoinChannelSuccess channelName: {0}, uid: {1}, elapsed: {2}",
                                 connection.channelId, connection.localUid, elapsed));
+            _audioSample.CreateLocalAudioCallQualityPanel();
         }
 
         public override void OnRejoinChannelSuccess(RtcConnection connection, int elapsed)
@@ -237,6 +330,7 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
         public override void OnLeaveChannel(RtcConnection connection, RtcStats stats)
         {
             _audioSample.Log.UpdateLog("OnLeaveChannel");
+            _audioSample.ClearAudioCallQualityPanel();
         }
 
         public override void OnClientRoleChanged(RtcConnection connection, CLIENT_ROLE_TYPE oldRole, CLIENT_ROLE_TYPE newRole, ClientRoleOptions newRoleOptions)
@@ -247,13 +341,83 @@ namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
         public override void OnUserJoined(RtcConnection connection, uint uid, int elapsed)
         {
             _audioSample.Log.UpdateLog(string.Format("OnUserJoined uid: ${0} elapsed: ${1}", uid, elapsed));
+            _audioSample.CreateRemoteAudioCallQualityPanel(uid);
         }
 
         public override void OnUserOffline(RtcConnection connection, uint uid, USER_OFFLINE_REASON_TYPE reason)
         {
             _audioSample.Log.UpdateLog(string.Format("OnUserOffLine uid: ${0}, reason: ${1}", uid,
                 (int)reason));
+            _audioSample.DestroyRemoteAudioCallQualityPanel(uid);
         }
+
+        //Quality monitoring during calls
+        public override void OnRtcStats(RtcConnection connection, RtcStats stats)
+        {
+            var panel = _audioSample.GetLocalAudioCallQualityPanel();
+            if (panel != null)
+            {
+                panel.Stats = stats;
+                panel.RefreshPanel();
+            }
+        }
+
+        public override void OnLocalAudioStats(RtcConnection connection, LocalAudioStats stats)
+        {
+            var panel = _audioSample.GetLocalAudioCallQualityPanel();
+            if (panel != null)
+            {
+                panel.AudioStats = stats;
+                panel.RefreshPanel();
+            }
+        }
+
+        public override void OnLocalAudioStateChanged(RtcConnection connection, LOCAL_AUDIO_STREAM_STATE state, LOCAL_AUDIO_STREAM_REASON reason)
+        {
+
+        }
+
+        public override void OnRemoteAudioStats(RtcConnection connection, RemoteAudioStats stats)
+        {
+            var panel = _audioSample.GetRemoteAudioCallQualityPanel(stats.uid);
+            if (panel != null)
+            {
+                panel.AudioStats = stats;
+                panel.RefreshPanel();
+            }
+        }
+
+        public override void OnRemoteAudioStateChanged(RtcConnection connection, uint remoteUid, REMOTE_AUDIO_STATE state, REMOTE_AUDIO_STATE_REASON reason, int elapsed)
+        {
+
+        }
+
+        public override void OnAudioVolumeIndication(RtcConnection connection, AudioVolumeInfo[] speakers, uint speakerNumber, int totalVolume)
+        {
+            foreach (var speaker in speakers)
+            {
+                if (speaker.uid == 0)
+                {
+                    var panel = _audioSample.GetLocalAudioCallQualityPanel();
+                    if (panel != null)
+                    {
+                        panel.Volume = (int)speaker.volume;
+                        panel.RefreshPanel();
+                    }
+                }
+                else
+                {
+                    var panel = _audioSample.GetRemoteAudioCallQualityPanel(speaker.uid);
+                    if (panel != null)
+                    {
+                        panel.Volume = (int)speaker.volume;
+                        panel.RefreshPanel();
+                    }
+                }
+            }
+
+        }
+
     }
 
     #endregion
---
+++ API-Example-Unity/Assets/API-Example/Examples/Basic/JoinChannelAudio/RemoteAudioCallQualityPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Agora.Rtc;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Agora_RTC_Plugin.API_Example.Examples.Basic.JoinChannelAudio
+{
+    public class RemoteAudioCallQualityPanel : MonoBehaviour
+    {
+
+        public uint Uid = 0;
+        public int Volume = -1;
+        public RemoteAudioStats AudioStats = null;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void RefreshPanel()
+        {
+            List<string> show = new List<string>();
+            show.Add(string.Format("Remote Uid: {0}", Uid));
+            if (Volume >= 0)
+            {
+                show.Add(string.Format("Volume: {0}", Volume));
+            }
+            if (AudioStats != null)
+            {
+                show.Add(string.Format("ARecv: {0}kbps", AudioStats.receivedBitrate));
+                show.Add(string.Format("ALoss: {0}%", AudioStats.audioLossRate));
+                show.Add(string.Format("AQuality: {0}", (QUALITY_TYPE)AudioStats.quality));
+            }
+            var text = this.GetComponentInChildren<Text>();
+            text.text = string.Join("\n", show.ToArray());
+        }
+    }
+}
